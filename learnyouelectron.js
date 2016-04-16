97e3983037b91489b63046f24db1303b9fb7126b
Justin M. Hill <justinmhill@gmail.com>
2016-04-16 15:21:56 -0400

    Fill in main executable with startup script

--- learnyouelectron.js
+++ learnyouelectron.js
@@ -1,1 +1,19 @@
 // Main executable goes here
+
+// #!/usr/bin/env node
+
+const workshopper = require('workshopper'),
+      path        = require('path')
+
+function fpath (f) {
+    return path.join(__dirname, f)
+}
+
+workshopper({
+    name        : 'learnyouelectron',
+    title       : 'learnyouelectron',
+    subtitle    : 'Learn the basics of cross-platform apps with electron',
+    appDir      : __dirname,
+    menuItems   : [],
+    exerciseDir : fpath('./exercises/')
+})
